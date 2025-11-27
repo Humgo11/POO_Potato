b4fef858941b6d22c79749de2c87a366db553485
Ghost <qpages08@gmail.com>
2025-11-27 12:20:43 +0100

    code initial

--- main.py
+++ main.py
@@ -1,1 +1,45 @@
-print("HelloWold")
+
+import pyxel
+
+
+class Game:
+    def __init__(self,width,height,nom_jeu):
+        self.width = width
+        self.height = height
+        self.nom = nom_jeu
+
+        pyxel.init(self.width, self.height)
+        self.x = 0
+        pyxel.run(self.update, self.draw)
+
+    def update(self):
+        pass
+    
+    def draw(self):
+        pyxel.cls(0)
+        pass
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+Game(128,128,"Game")
